c5fb0dd16d0fb5623389128ce95f72dbb3b0ced4
Erik <erik.benton@hotmail.com>
2017-10-28 20:42:54 -0400

    installing some postcss and npm packages to streamline gulp/css workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
-var gulp 	= require("gulp"),
-		watch = require("gulp-watch");
+var gulp 					= require("gulp"),
+		watch 				= require("gulp-watch"),
+		postcss 			= require("gulp-postcss"),
+		autoprefixer 	= require("autoprefixer"),
+		cssvars				= require("postcss-simple-vars"),
+		nested				= require("postcss-nested");
 
 gulp.task("watch", function(){
 	watch("./app/assets/styles/**/*.css", function(){
@@ -9,5 +13,6 @@ gulp.task("watch", function(){
 
 gulp.task("styles", function() {
 	return gulp.src('./app/assets/styles/styles.css')
+		.pipe(postcss([nested, cssvars, autoprefixer]))
 		.pipe(gulp.dest('./app/temp/styles'));
 });
